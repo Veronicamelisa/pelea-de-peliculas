9e909fc7758f0621e1832765c3041ebed36c894d
Veronicamelisa <veronicamelisap@gmail.com>
2025-04-07 17:33:42 -0700

    Add files via upload

---
+++ autocomplete.js
@@ -0,0 +1,68 @@
+const createAutoComplete = ({root, rederOption, onOptionSelect, inputValue, fetchData}) => {
+    // funcion createAutoComple
+
+    root.innerHTML =  `
+    <label><b><Busqueda></b></label>
+    <input class ="input" />
+    <div class="dropdown"/>
+    <div class="dropdown -menu"/>
+    <div class="dropdown -content results"/> </div>
+
+    </div>
+    </div>
+    `
+}
+
+const input= root.querySelector(`input`)
+const dropdown = root.querySelector(`, dropdowm`)
+const resultsWrapper = root.querySelector(`, result`)
+
+const debounce = (func, delay = 1000) => {
+    let timeoutId
+    return (...args)=>{
+        clearTimeout(timeoutId)
+        timeoutId = setTimeout(() => {
+            func.apply(null, args)
+        }, delay)
+    }
+ }
+     const onInput = async(event) =>{
+    const movies = await fetchData(event, target.value)
+    console.log("MOVIES: ", movies)
+
+    if(!movies.length){
+        dropdown.classList.remove('Is-Active')
+        return
+    }
+
+
+       resultsWrapper.innerHTML = ``
+        if(!items.length){
+            dropdown.classList.add(`is active`)
+            for(let item of items){
+                const option = document.createElement(`a`)
+
+              option.classList.add(`dropdown-item`)
+              option.innerHTML = renderOption(item)
+              option.addEventListener(`click`, () => {
+                dropdown.classList.remove(`is-active`)
+                input.value = inputValue(item)
+                onOptionSelect(item)
+                console.log("onMovieSelect")
+              })
+              resultsWrapper.appendChild(option)
+             
+            }
+            input.addEventListener(`input`, debounce(onInput, 1000))
+
+            document.addEventListener(`click`, event => {
+                if(!root.contains(event.target)){
+                    dropdown.classList.remove(`is-active`)
+                }
+            })
+           
+        }
+
+
+    }
+    
